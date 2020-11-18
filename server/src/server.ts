f83d3f3a2f1a483e0cca8dc0cb831c7db1569260
John Hopkins <JohnTHopkins@gmail.com>
2020-11-18 02:20:46 -0800

    restore server file

---
+++ server/src/server.ts
@@ -0,0 +1,83 @@
+import express from 'express'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import flash from 'express-flash'
+import path from 'path'
+import { SESSION_SECRET } from "./util/secrets";
+
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+        url: mongoUrl,
+        autoReconnect: true
+    })
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+    req.path !== "/login" &&
+    req.path !== "/signup" &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+    } else if (req.user &&
+    req.path == "/account") {
+        req.session.returnTo = req.path;
+    }
+    next();
+});
+
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", appServer)
+app.get("/login", userController.getLogin);
+app.post("/login", userController.postLogin);
+app.get("/logout", userController.logout);
+app.get("/forgot", userController.getForgot);
+app.post("/forgot", userController.postForgot);
+app.get("/reset/:token", userController.getReset);
+app.post("/reset/:token", userController.postReset);
+app.get("/signup", userController.getSignup);
+app.post("/signup", userController.postSignup);
+app.get("/contact", contactController.getContact);
+app.post("/contact", contactController.postContact);
+app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
+app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
+app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
+app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
+app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
+
+/**
+ * API examples routes.
+ */
+app.get("/api", apiController.getApi);
+app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
+
+/**
+ * OAuth authentication routes. (Sign in)
+ */
+app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
+app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
+    res.redirect(req.session.returnTo || "/");
+});
+
+export default app;
