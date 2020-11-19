d33a9098ef07c9ed8eccbda07515bae6f64f6687
John Hopkins <JohnTHopkins@gmail.com>
2020-11-19 13:17:06 -0800

    frontend changes

--- client/src/CreateDID.tsx
+++ client/src/CreateDID.tsx
@@ -1,27 +1,22 @@
 import React, {useState} from 'react'
 import {getWalletContents} from './utils'
-import {CreateOperation, SelectByImage, ISelectByImageProps} from '@material-did/core'
+import {CreateOperation, SelectByImage, GenerateContentsDialog, ICreateOperationProps } from '@material-did/core'
+import {Button} from '@material-ui/core'
+import {methods} from '@sidetree/wallet' 
 declare var window: any;
 
-const newDID = {
-    "@context": "https://w3id.org/did/v1",
-  "publicKey": [
-    {
-      "id": "#primary",
-      "type": "JsonWebKey2020",
-      "publicKeyJwk": {
-        "crv": "Ed25519",
-        "x": "2UR1Cz7qUSuoc4b4xw4JNJto1PD4IcTNC28Xdwrbdug",
-        "kty": "OKP"
-      }
-    }
-  ]
+interface SidetreeCreateOperation {
+  type: 'create';
+  suffix_data: string;
+  delta: string;
 }
 
 const CreateDID = (props: any) => {
   const [ walletState, setWalletState ] = useState({ contents: [] });
   const [ activeKey, setActiveKey ] = useState(null)
+  const [ mnemonic, setMnemonic ] = useState(null)
   const [ keyOptions, setKeyOptions] = useState<Array<any> | undefined>(undefined)
+  const [ createOperation, setCreateOperation ] = useState<SidetreeCreateOperation | undefined>(undefined)
   const checkForKeys = async () => {
     const { ethereum } = window;
     if (typeof ethereum !== "undefined") {
@@ -35,12 +30,52 @@ const CreateDID = (props: any) => {
     setKeyOptions(options)
   };
 
+  const generateKeyPair = async (mnemonic: (string | null)) => {
+    console.log(mnemonic)
+    const index = keyOptions ? keyOptions.length : 0
+    let createOP
+    if(!mnemonic == null){
+      createOP = await methods.getCreateOperationForProfile(mnemonic!, index )
+    }
+    setCreateOperation(createOP)
+    console.log(createOperation)
+  }
+  const CreateKeyPairButton = (componentProps: any) => {
+  return (
+    <div onClick={componentProps.onSubmit}>
+      <Button component='button' variant={componentProps.variant} color={componentProps.variant} onSubmit={componentProps.onSubmit} value={componentProps.value}>Create Key Pair</Button>
+    </div>
+  )
+}
+
   checkForKeys()
     return (
         <>
-            {(activeKey === null) && <SelectByImage label={'Active Keys'} value={activeKey} options={keyOptions} onChange={setActiveKey}/>}
-            {activeKey && <CreateOperation operation={} didMethodPrefix={'elem:ropsten'}/>}
+            { ((activeKey === null) && mnemonic == null) && 
+            <>
+            <SelectByImage label={'Active Keys'} value={activeKey} options={keyOptions} onChange={setActiveKey}/>
+            <br/>
+            OR 
+            <br/>
+            Generate a new Mnemonic
+            <br/>
+            <GenerateContentsDialog component={Button} variant="contained" color="secondary" onSubmit={setMnemonic}/>
+            </>
+            }
+            { mnemonic && 
+              <CreateKeyPairButton 
+                componentProps={{
+                  variant: 'contained',
+                  color: 'primary',
+                  onSubmit: generateKeyPair,
+                  value: mnemonic
+                }} 
+              />
+            }
+            
+            {createOperation && <CreateOperation operation={createOperation} didMethodPrefix={'elem:ropsten'}/>}
         </>
     )
 }
 export default CreateDID
+
